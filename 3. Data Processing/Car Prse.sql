2170f79b9ffb381ecb442475f52937ee1dc965bf
SharonM-Analyst <sshanay92@gmail.com>
2026-05-02 12:34:29 +0000

    GIT EXAMPLE

--- 3. Data Processing/Car Prse.sql
+++ 3. Data Processing/Car Prse.sql
@@ -3,5 +3,6 @@ SELECT * FROM `workspace`.`default`.`cars`;
 
 SELECT 
 MIN(sellingprice)  lowest_price,
-MAX(sellingprice )   highest_price
+MAX(sellingprice )   highest_price,
+COUNT(vin)           Total_cars
 FROM  `workspace`.`default`.`cars`;
